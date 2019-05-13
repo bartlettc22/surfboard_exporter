704f00da5215c21f9ee8675db609d7ff261e0a4a
Chris Bartlett <bartlettc@gmail.com>
2019-05-12 22:19:44 -0600

    fixing build

--- scripts/publish_docker.sh
+++ scripts/publish_docker.sh
@@ -5,5 +5,10 @@ set -eo pipefail
 VERSION=${1:-dev}
 DOCKER_REPO="bartlettc/surfboard-exporter"
 
+if [[ "${VERSION}" == "master" ]]; then
+  docker tag ${DOCKER_REPO}:master ${DOCKER_REPO}:latest
+  VERSION="latest"
+fi
+
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push ${DOCKER_REPO}:${VERSION}
